1f2fa380726aa3e70541c379d51d9e563ca5ea62
Fearsomemaster <83412879+Fearsomemaster@users.noreply.github.com>
2021-11-23 11:31:50 +0530

    Create Q2_Highest_Marks_2.py

--- Q2_Highest_Marks_2.py
+++ Q2_Highest_Marks_2.py
@@ -2,17 +2,13 @@
    Program to find the roll no of a student who scored highest marks
 '''
 marks=[78,89,85,91,93,82,79]
-roll_nos=[1,2,3,4,5,6,7]
 Marks_index=1
 Max_marks=marks[0]
+S_rollno=1
+count=0
 for Marks_index in marks:
     if Marks_index > Max_marks:
         Max_marks=Marks_index
-count=0
-value=0
-for value in marks:
-    if(value==Max_marks):
-        S_rollno=count
+        S_rollno=count+1
     count+=1
-S_rollno=roll_nos[S_rollno]
 print("Roll no of Student who scored highest marks is:-",S_rollno)  
